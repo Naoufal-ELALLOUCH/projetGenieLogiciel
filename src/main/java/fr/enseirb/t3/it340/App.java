9f962a93baea69ff1302acaf824f7807b4f2e6ba
charllie <charlie59320@gmail.com>
2015-11-27 17:52:20 +0100

    Calcul de la couverture

--- src/main/java/fr/enseirb/t3/it340/App.java
+++ src/main/java/fr/enseirb/t3/it340/App.java
@@ -3,6 +3,7 @@ import static spark.Spark.*;
 
 import fr.enseirb.t3.it340.bdd.BddConnecteur;
 import fr.enseirb.t3.it340.servlets.VisualisationAteliers;
+import fr.enseirb.t3.it340.servlets.ateliers.VisualisationAtelier;
 import fr.enseirb.t3.it340.servlets.authentification.Authentification;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ import java.sql.SQLException;
 public class App {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
-		post("/authentification", new Authentification());
+
 		get("/ateliers", new VisualisationAteliers());
+
+		get("/atelier/:idAtelier", new VisualisationAtelier());
+
+		post("/authentification", new Authentification());
 
 	}
 
