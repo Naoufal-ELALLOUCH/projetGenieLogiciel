361e4ab6c74176017f3bea43e677c331ce767939
Naoufal EL ALLOUCH <naoufal.elallouch@gmail.com>
2015-12-06 00:14:35 +0100

    ajout des classes de Test de BddAtelier et BddCreneau

---
+++ src/test/java/fr/enseirb/t3/it340/bdd/TestBddAtelier.java
@@ -0,0 +1,24 @@
+package fr.enseirb.t3.it340.bdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBddAtelier {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
