c6dca220f5745598a164e4b5d412aefd2932eb1d
hhe <heloise.hour@orange.fr>
2015-12-07 19:46:51 +0100

    ajout enregistrement + test

--- src/main/java/fr/enseirb/t3/it340/bdd/BddEnregistrement.java
+++ src/main/java/fr/enseirb/t3/it340/bdd/BddEnregistrement.java
@@ -13,27 +13,78 @@ import fr.enseirb.t3.it340.modeles.Utilisateur;
 public class BddEnregistrement {
 	private static final Logger log = LoggerFactory.getLogger(BddEnregistrement.class);
 
-	public static void enregistrement(int idEnseignant, int idCreneau, int nbInscrits ) {
-		
-		//String sql = "SELECT email, motDePasse FROM Utilisateur WHERE email=? AND motDePasse=?";
-
-		
+	public static void enregistrement(int idEnseignant, int idCreneau, int nbAInscrire ) {
 		try {
-			Connection connection = BddConnecteur.getConnection();
-			String sql = "INSERT INTO Enregistrement(idEnseignant, idCreneau,nbInscrits) VALUES(?,?)";
+		Connection connection = BddConnecteur.getConnection();
 
-			PreparedStatement statement = connection.prepareStatement(sql);
-			statement.setInt(1, idEnseignant);
-			statement.setInt(2, idCreneau);
-			statement.setInt(3, nbInscrits);
+		String sql1 = "SELECT capacite FROM Creneau WHERE idCreneau = ? ";
+		PreparedStatement statement1 = connection.prepareStatement(sql1);
+		statement1.setInt(1, idCreneau);
+		
+		String sql2 = "SELECT nbInscrits FROM Enregistrement WHERE idCreneau = ? AND idEnseignant = ? ";
+		PreparedStatement statement2 = connection.prepareStatement(sql2);
+		statement2.setInt(1, idCreneau);
+		statement2.setInt(2, idEnseignant);
 
-			statement.executeUpdate();
 
-			statement.close();
-			connection.close();
-		} catch (Exception e) {
-			log.error("Impossible d'insérer l'enregistrement d'un enseignant à un créneau : {}", e);
+		//statement.executeQuery();
+		ResultSet rs1 = statement1.executeQuery();
+		ResultSet rs2 = statement2.executeQuery();
+
+		int capacite = 0;
+		int nbInscrits = 0;
+		int count = 0;
+		while(rs1.next()) {
+			capacite = rs1.getInt("capacite");
 		}
+		while(rs2.next()) {
+			nbInscrits = rs2.getInt("nbInscrits");
+			count++;
+		}
+
+		statement1.close();
+		statement2.close();
+		connection.close();
+			try {
+				connection = BddConnecteur.getConnection();
+
+				if (count==0){
+					String sql = "INSERT INTO Enregistrement(idEnseignant, idCreneau,nbInscrits) VALUES(?,?,?)";
+	
+					PreparedStatement statement = connection.prepareStatement(sql);
+					statement.setInt(1, idEnseignant);
+					statement.setInt(2, idCreneau);
+					statement.setInt(3, nbAInscrire);
+					
+					if(nbInscrits+nbAInscrire<=capacite)
+						statement.executeUpdate();
+
+					statement.close();
+					connection.close();
+				}
+				if (count!=0){
+					String sql = "UPDATE Enregistrement SET nbInscrits = ? WHERE idEnseignant = ? AND idCreneau = ?";
+	
+					PreparedStatement statement = connection.prepareStatement(sql);
+					statement.setInt(1, nbAInscrire+nbInscrits);
+					statement.setInt(2, idEnseignant);
+					statement.setInt(3, idCreneau);
+				
+					if(nbInscrits+nbAInscrire<=capacite)
+						statement.executeUpdate();
+					
+					statement.close();
+					connection.close();
+				}
+
+			} catch (Exception e) {
+				log.error("Impossible d'insérer l'enregistrement d'un enseignant à un créneau : {}", e);
+			}
+		} catch (Exception e) {
+			log.error("Impossible de récupérer le nombre de place restante : {}", e);
+		}
+		
+
 	}
 	
 //	public static List<Integer> getListeIdEnseignantByIdCreneau(String email) {
--- src/test/java/fr/enseirb/t3/it340/bdd/TestBddEnregistrement.java
+++ src/test/java/fr/enseirb/t3/it340/bdd/TestBddEnregistrement.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.junit.After;
 import org.junit.Test;
 
 public class TestBddEnregistrement {
@@ -19,27 +20,87 @@ public class TestBddEnregistrement {
 		int idAtelier = 1;
 		String jour = "12/05/2015";
 		String heure = "13:00";
-		int oldCapacite = 20;
-		int newCapacite = 40;
-		BddCreneau.ajoutCreneau(1, jour, heure, oldCapacite);
-		BddEnseignant.ajout(1, "nom", "prenom");
+		int capacite = 30;
+		BddUtilisateur.ajout("labri@labri.fr", "labri");
+		BddLabo.ajout(1, "Labri");
+		BddAtelier.ajoutAtelier(1, "A la poursuite d'ennemis invisibles", "Sciences de la vie ", "Campus Carreire (Hôpital Pellegrin)", "Labo MFP", "","", "","");
+		BddCreneau.ajoutCreneau(1, jour, heure, capacite);
 
+		BddUtilisateur.ajout("enseignant@labri.fr", "labri");
+		BddEnseignant.ajout(2, "nom", "prenom");
+
+		//Insertion d'un nombre correct
 		BddEnregistrement.enregistrement(1, 1, 20);
+		
 		// Vérification
-		String sql = "SELECT idLabo FROM Atelier WHERE idLabo=1";
+		String sql = "SELECT nbInscrits FROM Enregistrement WHERE idEnregistrement=1";
 		Statement statement = connection.createStatement();
 		ResultSet rs = statement.executeQuery(sql);
 
 		int count = 0;
+		int nbInscrits = 0;
 		while(rs.next()) {
+			nbInscrits = rs.getInt("nbInscrits");
 			count++;
 		}
-
+		
 		assertEquals(count, 1);
+		assertEquals(nbInscrits, 20 );
+		
+		//Insertion d'un nombre égal à la capcité
+				BddEnregistrement.enregistrement(1, 1, 10);
+				
+				// Vérification
+				sql = "SELECT nbInscrits FROM Enregistrement WHERE idEnregistrement=1";
+				statement = connection.createStatement();
+				rs = statement.executeQuery(sql);
+
+				count = 0;
+				nbInscrits = 0;
+				while(rs.next()) {
+					nbInscrits = rs.getInt("nbInscrits");
+					count++;
+				}
+
+				assertEquals(count, 1);
+				assertEquals(nbInscrits, 30 );
+				
+				//Insertion d'un nombre supérieur à la capcité
+				BddEnregistrement.enregistrement(1, 1, 10);
+				
+				// Vérification
+				sql = "SELECT nbInscrits FROM Enregistrement WHERE idEnregistrement=1";
+				statement = connection.createStatement();
+				rs = statement.executeQuery(sql);
+
+				count = 0;
+				nbInscrits = 0;
+				while(rs.next()) {
+					nbInscrits = rs.getInt("nbInscrits");
+					count++;
+				}
+
+				assertEquals(count, 1);
+				assertEquals(nbInscrits, 30 );
 
 		// Fermeture
 		rs.close();
 		statement.close();
 		connection.close();
+	}
+	@After
+	public void dispose() throws SQLException, IOException, ClassNotFoundException {
+		Connection connection = BddConnecteur.getConnection();
+		Statement statement = connection.createStatement();
+		statement.execute("DROP TABLE Creneau");
+		statement.execute("DROP TABLE Enseignant");
+		statement.execute("DROP TABLE Enregistrement");
+		statement.execute("DROP TABLE Atelier");
+		statement.execute("DROP TABLE Utilisateur");
+		statement.execute("DROP TABLE Labo");
+
+		statement.close();
+		connection.close();
+		BddConnecteur.dispose();
 	}
 }
