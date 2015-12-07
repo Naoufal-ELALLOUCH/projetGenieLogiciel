537a2b68d80041ff5da9644ff83b6e0677a34569
charllie <charlie59320@gmail.com>
2015-12-07 15:57:17 +0100

    BddCreneau + TestBddCreneau

--- src/main/java/fr/enseirb/t3/it340/bdd/BddCreneau.java
+++ src/main/java/fr/enseirb/t3/it340/bdd/BddCreneau.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
@@ -18,6 +19,18 @@ public class BddCreneau {
 
 	private static final Logger log = LoggerFactory.getLogger(BddCreneau.class);
 
+	public static java.sql.Date stringToDate(String stringDate) throws ParseException {
+		// String to sqlDate
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		Date parsed = format.parse(stringDate);
+		return new java.sql.Date(parsed.getTime());
+	}
+
+	public static java.sql.Time stringToTime(String stringTime) throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+		return new java.sql.Time(formatter.parse(stringTime).getTime());
+	}
+
 	// Ajout d'un creneau
 	public static void ajoutCreneau(int idAtelier, String jour, String heure, int capacite) {
 
@@ -27,18 +40,9 @@ public class BddCreneau {
 			Connection connection = BddConnecteur.getConnection();
 			PreparedStatement statement = connection.prepareStatement(sql);
 
-			// String to sqlDate
-			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-			Date parsed = format.parse(jour);
-			java.sql.Date sqlDate = new java.sql.Date(parsed.getTime());
-
-			// String to sqlTime
-			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
-			java.sql.Time timeValue = new java.sql.Time(formatter.parse(heure).getTime());
-
 			statement.setInt(1, idAtelier);
-			statement.setDate(2, sqlDate);
-			statement.setTime(3, timeValue);
+			statement.setDate(2, stringToDate(jour));
+			statement.setTime(3, stringToTime(heure));
 			statement.setInt(4, capacite);
 
 			statement.executeUpdate();
@@ -52,20 +56,20 @@ public class BddCreneau {
 	}
 
 	// Editer un creneau
-	public static void editCreneau(int idCreneau, String jour ,String heure ,int capacite) {
+	public static void editCreneau(int idCreneau, String jour, String heure, int capacite) {
 
-		String sql = "UPDATE Creneau SET (jour=? , heure=? , capacite=?) WHERE idCreneau=?";
+		String sql = "UPDATE Creneau SET jour=?, heure=?, capacite=? WHERE idCreneau=?";
 		
 		try {
 			Connection connection = BddConnecteur.getConnection();
 			PreparedStatement statement = connection.prepareStatement(sql);
 
-			statement.setString(1, jour);
-			statement.setString(2, heure);
+			statement.setDate(1, stringToDate(jour));
+			statement.setTime(2, stringToTime(heure));
 			statement.setInt(3, capacite);
 			statement.setInt(4, idCreneau);
 
-			statement.executeQuery();
+			statement.executeUpdate();
 			statement.close();
 			connection.close();
 
@@ -87,7 +91,7 @@ public class BddCreneau {
 
 			statement.setInt(1, idCreneau);
 
-			statement.executeQuery();
+			statement.executeUpdate();
 			statement.close();
 			connection.close();
 
--- src/test/java/fr/enseirb/t3/it340/bdd/TestBddCreneau.java
+++ src/test/java/fr/enseirb/t3/it340/bdd/TestBddCreneau.java
@@ -15,14 +15,16 @@ public class TestBddCreneau {
 	public void testAjoutCreneau() throws SQLException, IOException, ClassNotFoundException {
 		Connection connection = BddConnecteur.getConnection();
 
-		//BddAtelier.ajoutAtelier();
+		BddUtilisateur.ajout("labri@labri.fr", "labri");
+		BddLabo.ajout(1, "Labri");
+		BddAtelier.ajoutAtelier(1, "A la poursuite d'ennemis invisibles", "Sciences de la vie ", "Campus Carreire (Hôpital Pellegrin)", "Labo MFP", "","", "","",null);
 
 		// Insertion
 		int idAtelier = 1;
 		String jour = "12/05/2015";
 		String heure = "13:00";
 		int capacite = 20;
-		BddCreneau.ajoutCreneau(idAtelier, jour, heure, capacite);
+		BddCreneau.ajoutCreneau(1, jour, heure, capacite);
 
 		// Vérification
 		String sql = "SELECT * FROM Creneau WHERE idAtelier='"+ idAtelier +"'";
@@ -38,7 +40,6 @@ public class TestBddCreneau {
 			jourRecup = rs.getDate("jour");
 			heureRecup = rs.getTime("heure");
 			capaciteRecup = rs.getInt("capacite");
-			System.out.println(heureRecup.toString());
 			count++;
 		}
 
@@ -54,11 +55,93 @@ public class TestBddCreneau {
 		connection.close();
 	}
 
+	@Test
+	public void testEditCreneau() throws SQLException, IOException, ClassNotFoundException {
+		Connection connection = BddConnecteur.getConnection();
+
+		BddUtilisateur.ajout("labri@labri.fr", "labri");
+		BddLabo.ajout(1, "Labri");
+		BddAtelier.ajoutAtelier(1, "A la poursuite d'ennemis invisibles", "Sciences de la vie ", "Campus Carreire (Hôpital Pellegrin)", "Labo MFP", "","", "","",null);
+
+		// Insertion
+		int idAtelier = 1;
+		String jour = "12/05/2015";
+		String heure = "13:00";
+		int oldCapacite = 20;
+		int newCapacite = 40;
+		BddCreneau.ajoutCreneau(1, jour, heure, oldCapacite);
+
+		// Edition
+		BddCreneau.editCreneau(1, jour, heure, newCapacite);
+
+		// Vérification
+		String sql = "SELECT * FROM Creneau WHERE idAtelier='"+ idAtelier +"'";
+		Statement statement = connection.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+
+		int count = 0;
+		int capaciteRecup = 0;
+
+		while(rs.next()) {
+			capaciteRecup = rs.getInt("capacite");
+			count++;
+		}
+
+		assertEquals(count, 1);
+		assertEquals(capaciteRecup, newCapacite);
+
+		// Fermeture
+		rs.close();
+		statement.close();
+		connection.close();
+	}
+
+	@Test
+	public void testSupprCreneau() throws SQLException, IOException, ClassNotFoundException {
+		Connection connection = BddConnecteur.getConnection();
+
+		BddUtilisateur.ajout("labri@labri.fr", "labri");
+		BddLabo.ajout(1, "Labri");
+		BddAtelier.ajoutAtelier(1, "A la poursuite d'ennemis invisibles", "Sciences de la vie ", "Campus Carreire (Hôpital Pellegrin)", "Labo MFP", "","", "","",null);
+
+		// Insertion
+		int idAtelier = 1;
+		String jour = "12/05/2015";
+		String heure = "13:00";
+		int oldCapacite = 20;
+		int newCapacite = 40;
+		BddCreneau.ajoutCreneau(1, jour, heure, oldCapacite);
+
+		// Suppression
+		BddCreneau.supprCreneau(1);
+
+		// Vérification
+		String sql = "SELECT * FROM Creneau WHERE idAtelier='"+ idAtelier +"'";
+		Statement statement = connection.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+
+		int count = 0;
+
+		while(rs.next()) {
+			count++;
+		}
+
+		assertEquals(count, 0);
+
+		// Fermeture
+		rs.close();
+		statement.close();
+		connection.close();
+	}
+
 	@After
 	public void dispose() throws SQLException, IOException, ClassNotFoundException {
 		Connection connection = BddConnecteur.getConnection();
 		Statement statement = connection.createStatement();
 		statement.execute("DROP TABLE Creneau");
+		statement.execute("DROP TABLE Atelier");
+		statement.execute("DROP TABLE Labo");
+		statement.execute("DROP TABLE Utilisateur");
 		statement.close();
 		connection.close();
 		BddConnecteur.dispose();
