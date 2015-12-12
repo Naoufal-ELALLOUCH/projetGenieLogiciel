e0ddf8f21b5171fe80913cc24427d391e331165d
hhe <heloise.hour@orange.fr>
2015-12-12 17:46:17 +0100

    ajout des tests testGetEnseignantByIdUtilisateur et testGetEnseignantByIdEnseignant

--- src/test/java/fr/enseirb/t3/it340/bdd/TestBddEnseignant.java
+++ src/test/java/fr/enseirb/t3/it340/bdd/TestBddEnseignant.java
@@ -11,6 +11,9 @@ import java.sql.Statement;
 import org.junit.After;
 import org.junit.Test;
 
+import fr.enseirb.t3.it340.modeles.Enseignant;
+import fr.enseirb.t3.it340.modeles.Utilisateur;
+
 public class TestBddEnseignant {
 	@Test
 	public void testAjout() throws SQLException, IOException, ClassNotFoundException {
@@ -47,6 +50,52 @@ public class TestBddEnseignant {
 	}
 
 	@Test
+	public void testGetEnseignantByIdEnseignant() throws SQLException, IOException, ClassNotFoundException {
+		Connection connection = BddConnecteur.getConnection();
+
+		Enseignant enseignant;
+		String nom = "nom";
+		String prenom = "prenom";
+		BddUtilisateur.ajout("labri@labri.fr", "labri");
+
+		// Test : récupération d'un objet null
+		enseignant = BddEnseignant.getEnseignantByIdEnseignant(1);
+		assertNull(enseignant);
+
+		// Insertion
+		BddEnseignant.ajout(1,nom, prenom);
+
+		// Test : récupération d'un utilisateur à partir de IdUtilisateur
+		enseignant = BddEnseignant.getEnseignantByIdEnseignant(1);
+		assertNotNull(enseignant);
+		assertEquals(enseignant.getNom(), nom);
+		assertEquals(enseignant.getPrenom(), prenom);
+	}
+	
+	@Test
+	public void testGetEnseignantByIdUtilisateur() throws SQLException, IOException, ClassNotFoundException {
+		Connection connection = BddConnecteur.getConnection();
+
+		Enseignant enseignant;
+		String nom = "nom";
+		String prenom = "prenom";
+		BddUtilisateur.ajout("labri@labri.fr", "labri");
+
+		// Test : récupération d'un objet null
+		enseignant = BddEnseignant.getEnseignantByIdUtilisateur(1);
+		assertNull(enseignant);
+
+		// Insertion
+		BddEnseignant.ajout(1,nom, prenom);
+
+		// Test : récupération d'un utilisateur à partir de IdUtilisateur
+		enseignant = BddEnseignant.getEnseignantByIdUtilisateur(1);
+		assertNotNull(enseignant);
+		assertEquals(enseignant.getIdEnseignant(), 1);
+		assertEquals(enseignant.getNom(), nom);
+		assertEquals(enseignant.getPrenom(), prenom);
+	}
+	@Test
 	public void testIsEnseignant() throws SQLException, IOException, ClassNotFoundException {
 		Connection connection = BddConnecteur.getConnection();
 
@@ -63,6 +112,8 @@ public class TestBddEnseignant {
 
 		connection.close();
 	}
+	
+	
 
 	@After
 	public void dispose() throws SQLException, IOException, ClassNotFoundException {
